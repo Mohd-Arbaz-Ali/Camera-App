70e5f0a80ffccb46525bf79a8da2e32dcd4adb9b
Mohd Arbaz Ali <mohdarbaz068@gmail.com>
2023-09-25 01:33:18 +0530

    first commit

---
+++ galley.js
@@ -0,0 +1,125 @@
+const backBtn = document.querySelector(".back");
+
+backBtn.addEventListener("click", function() {
+    location.assign("./index.html");
+});
+
+setTimeout(() => {
+    if(db) {
+        let galleryCont = document.querySelector(".media-cont-box");
+        let imageDBTransaction = db.transaction("image", "readonly");
+        let imageStore = imageDBTransaction.objectStore("image");
+        let imageRequest = imageStore.getAll();
+        imageRequest.onsuccess = function() {
+            if(imageRequest.result !== undefined) {
+                // imageRequest.result -> returns the objects of all images in an array.
+                // console.log("Images", imageRequest.result);
+                let imageResult = imageRequest.result;
+                imageResult.forEach((imageObj) => {
+                    let url = imageObj.url;
+                    // Create a image container.
+                    let imageEle = document.createElement("div");
+                    imageEle.setAttribute("class", "media-cont");
+                    imageEle.setAttribute("id", imageObj.id);
+                    // Add image to that container.
+                    imageEle.innerHTML = `
+                        <div class="media">
+                            <img src="${url}">
+                        </div>
+                        <div class="delete action-btn">DELETE</div>
+                        <div class="download action-btn">DOWNLOAD</div>
+                    `;
+                    // Append child in gallery container.
+                    galleryCont.appendChild(imageEle);
+
+                    let deleteBtn = imageEle.querySelector(".delete");
+                    deleteBtn.addEventListener("click", deleteListener);
+
+                    let downloadBtn = imageEle.querySelector(".download");
+                    downloadBtn.addEventListener("click", downloadListener);
+                });
+            } else {
+                // console.log("No Image Found!");
+            }
+        }
+
+        let videoDBTransaction = db.transaction("video", "readonly");
+        let videoStore = videoDBTransaction.objectStore("video");
+        let videoRequest = videoStore.getAll();
+        videoRequest.onsuccess = () => {
+            if(videoRequest.result !== undefined) {
+                let videoResult = videoRequest.result;
+                videoResult.forEach((videoObj) => {
+                    let videoEle  = document.createElement("div");
+                    videoEle.setAttribute("class", "media-cont");
+                    videoEle.setAttribute("id", videoObj.id);
+                    let url = URL.createObjectURL(videoObj.blobData);
+                    videoEle.innerHTML = `
+                        <div class="media">
+                            <video autoplay muted loop src="${url}">
+                        </div>
+                        <div class="delete action-btn">DELETE</div>
+                        <div class="download action-btn">DOWNLOAD</div>
+                    `;
+                    // Append child in gallery container.
+                    galleryCont.appendChild(videoEle);
+                    
+                    let deleteBtn = videoEle.querySelector(".delete");
+                    deleteBtn.addEventListener("click", deleteListener);
+
+                    let downloadBtn = videoEle.querySelector(".download");
+                    downloadBtn.addEventListener("click", downloadListener);
+                })
+            } else {
+                // console.log("No Video Found!");
+            }
+        }
+    }
+}, 100);
+
+function deleteListener(event) {
+    // Get id from event.
+    let id = event.target.parentElement.getAttribute("id");
+    // console.log(id);
+    // Find id belongs to which store.
+    let mediaType = id.split("-")[0];
+    // Go into the db of video/image.
+    // Delete it.
+    if(mediaType == "img") {
+        let imageDBTransaction = db.transaction("image", "readwrite");
+        let imageStore = imageDBTransaction.objectStore("image");
+        imageStore.delete(id);
+    } else { // Video
+        let videoDBTransaction = db.transaction("video", "readwrite");
+        let videoStore = videoDBTransaction.objectStore("video");
+        videoStore.delete(id);
+    }
+    // Delete from frontend.
+    event.target.parentElement.remove();
+}
+
+function downloadListener(event) {
+    // Get id from event.
+    let id = event.target.parentElement.getAttribute("id");
+    // Find id belongs to which store.
+    let mediaType = id.split("-")[0];
+    // Go into the db of video/image.
+    // Download it.
+    if(mediaType == "img") {
+        // Get url from video.
+        let imageURL = event.target.parentElement.querySelector("img").getAttribute("src");
+        let a = document.createElement("a");
+        a.href = imageURL;
+        a.download = `${id}.png`;
+        a.click();
+        a.remove();
+    } else { // Video
+        // Get url from video.
+        let videoURL = event.target.parentElement.querySelector("video").getAttribute("src");
+        let a = document.createElement("a");
+        a.href = videoURL;
+        a.download = `${id}.mp4`;
+        a.click();
+        a.remove();
+    }
+}
---
+++ script.js
@@ -0,0 +1,164 @@
+// alert("Working . . .");
+const recordBtn = document.querySelector(".record-circle");
+const captureBtn = document.querySelector(".capture-circle");
+const timerBox = document.querySelector(".timer-box p");
+const video = document.querySelector(".camera-view");
+const filterLayer = document.querySelector(".filter-layer");
+const allFilters = document.querySelectorAll(".filter");
+const filterCont = document.querySelector(".filter-layer");
+const galleryBtn = document.querySelector(".fa-photo-film");
+const uid = new ShortUniqueId();
+let filterColor = "transparent";
+let mediaRecorder;
+let transaction;
+
+// This will store the video recording stream.
+let chunks = [];
+let constraints = {
+    audio: true,
+    video: true
+}
+navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+    video.srcObject = stream;
+    filterCont.style.aspectRatio = stream.getVideoTracks()[0].getSettings().aspectRatio;
+    mediaRecorder = new MediaRecorder(stream);
+    mediaRecorder.addEventListener("start", () => {
+        chunks = [];
+        // console.log("Start Recording. . .");
+    })
+    mediaRecorder.addEventListener("dataavailable", (event) => {
+        chunks.push(event.data);
+    })
+    mediaRecorder.addEventListener("stop", () => {
+        // console.log("Stopped Recording!");
+        let blob = new Blob(chunks, {type: "video/mp4"});
+        let videoURL = URL.createObjectURL(blob);
+        // console.log(videoURL);
+        // Getting recorded video.
+        // let a = document.createElement("a");
+        // a.href = videoURL;
+        // a.download = "myVideo.mp4";
+        // a.click();
+        if(db) {
+            // console.log("working. . .");
+            let videoID = uid.rnd();
+            transaction = db.transaction("video", "readwrite");
+            let videoStore = transaction.objectStore("video");
+            let videoEntry = {
+                id: `vid-${videoID}`,
+                blobData: blob
+            }
+            let addRequest = videoStore.add(videoEntry);
+            addRequest.onsuccess = function() {
+                // console.log("Video entry added to the video store!", addRequest.result);
+            }
+            addRequest.onerror = function() {
+                // console.log("Video entry NOT added to the video store!", addRequest.error);
+            }
+        }
+    })
+})
+
+let isRecording = false;
+recordBtn.addEventListener("click", function() {
+    var audio = new Audio("./sounds/record.mp3");
+    audio.play();
+    if(!isRecording) {
+        mediaRecorder.start();
+        recordBtn.classList.add("scale-record");
+        timerBox.style.visibility = "visible";
+        startTimer()
+    } else {
+        mediaRecorder.stop();
+        recordBtn.classList.remove("scale-record");
+        timerBox.style.visibility = "hidden";
+        stopTimer();
+    }
+    isRecording = !isRecording;
+});
+
+let timerID;
+function startTimer() {
+    let counter = 1;
+    function displayTimer() {
+        let totalSeconds = counter;
+
+        let hours = parseInt(totalSeconds / 3600);
+        totalSeconds = totalSeconds % 3600;
+        let mins = parseInt(totalSeconds / 60);
+        totalSeconds = totalSeconds % 60;
+        let secs = totalSeconds;
+
+        hours = (hours < 10)? `0${hours}`: hours;
+        mins = (mins < 10)? `0${mins}`: mins;
+        secs = (secs < 10)? `0${secs}`: secs;
+
+        timerBox.innerText = `${hours}:${mins}:${secs}`;
+        counter += 1;
+    };
+    timerID = setInterval(displayTimer, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerID); 
+    timerBox.innerHTML = "00:00:00";
+}
+
+captureBtn.addEventListener("click", function() {
+    var audio = new Audio("./sounds/capture.mp3");
+    audio.play();
+    if(!isRecording) {
+        captureBtn.classList.add("scale-capture");
+        // Capturing image using canvas.
+        let canvas = document.createElement("canvas");
+        let ctx = canvas.getContext("2d");
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = filterColor;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        // Auto Download the image.
+        let imageURL = canvas.toDataURL("image/jpeg");
+        // let a = document.createElement("a");
+        // a.href = imageURL;
+        // a.download = "myPic.jpeg"
+        // a.click();
+        if(db) {
+            // console.log("working. . .");
+            let imageID = uid.rnd();
+            transaction = db.transaction("image", "readwrite");
+            let imageStore = transaction.objectStore("image");
+            let imageEntry = {
+                id: `img-${imageID}`,
+                url: imageURL
+            }
+            let addRequest = imageStore.add(imageEntry);
+            addRequest.onsuccess = function() {
+                // console.log("Image entry added to the image store!", addRequest.result);
+            }
+            addRequest.onerror = function() {
+                // console.log("Image entry NOT added to the video store!", addRequest.error);
+            }
+        }
+
+        setTimeout(() => {
+            captureBtn.classList.remove("scale-capture");
+        }, 1000);
+    }
+});
+
+allFilters.forEach((currFilter) => {
+    currFilter.addEventListener("click", function() {
+        filterColor = getComputedStyle(currFilter).getPropertyValue("background-color");
+        allFilters.forEach(filter => filter.style.border = "none");
+        // Need to test.
+        filterLayer.style.backgroundColor = filterColor;
+        currFilter.style.border = "2px solid red";
+    });
+})
+
+galleryBtn.addEventListener("click", function() {
+    location.assign("./gallery.html");
+})
